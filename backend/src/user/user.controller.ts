3ebdc8ef96aa5c7b82a5b896085590bd44d9f8ca
Oleksandr Somov <asomov@mindk.com>
2023-03-03 17:21:19 +0200

    add create user and auth and upload file

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UserService } from './user.service';
+import { UserCreateDto } from './dtos/user-create.dto';
+import { User } from './user.entity';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Post()
+  create(@Body() createUser: UserCreateDto) {
+    return this.userService.create(createUser);
+  }
+
+  @Post('avatar')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadAvatar(@UploadedFile() avatar: Express.Multer.File) {
+    return this.userService.updateAvatar(avatar);
+  }
+}
